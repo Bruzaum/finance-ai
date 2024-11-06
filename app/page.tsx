57cf3fa4f6a4786cfd2d127e9ffedd693afb19e8
Bruno Santarem <santarembrunno@gmail.com>
2024-11-06 12:50:38 -0300

    feat: add authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen dark">
-      <Button>Teste Botao</Button>
-    </div>
-  );
-}
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return <UserButton showName />;
+};
+
+export default Home;
